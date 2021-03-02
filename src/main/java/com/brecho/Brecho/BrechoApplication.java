b234b7e7c90763ca1d087e08be6f0fe3c1aea56b
Pedro <pedrowarsps4@gmail.com>
2021-03-02 16:24:00 -0300

    retirei os getter e setter e adicionei a Dependencia Lombok, anotações @getters @setters

--- src/main/java/com/brecho/Brecho/BrechoApplication.java
+++ src/main/java/com/brecho/Brecho/BrechoApplication.java
@@ -4,13 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.brecho.Brecho.model.Brecho;
+import com.brecho.Brecho.model.Categoria;
+import com.brecho.Brecho.model.Produto;
+import com.brecho.Brecho.model.Usuario;
 
 @SpringBootApplication
 public class BrechoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BrechoApplication.class, args);			
-		
+		SpringApplication.run(BrechoApplication.class, args);	
+			
 	}
 	
 }
--- src/main/java/com/brecho/Brecho/model/Categoria.java
+++ src/main/java/com/brecho/Brecho/model/Categoria.java
@@ -13,45 +13,27 @@ import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-@Entity
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity @NoArgsConstructor @AllArgsConstructor
 public class Categoria {
 
-	@Id
+	@Id 
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Getter
 	private Long id;
 
-	@NotNull
+	@NotNull 
+	@Getter @Setter
 	@Size(min = 3, max = 50, message = "Mínimo de 3 caracteres e máximo de 50.")
 	private String tipo;
 
 	@OneToMany(mappedBy = "categoria", cascade = CascadeType.ALL)
 	@JsonIgnoreProperties("categoria")
-	private List<Produto> produtos;
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getTipo() {
-		return tipo;
-	}
-
-	public void setTipo(String tipo) {
-		this.tipo = tipo;
-	}
-
-	public List<Produto> getProdutos() {
-		return produtos;
-	}
-
-	public void setProdutos(List<Produto> produtos) {
-		this.produtos = produtos;
-	}
-	
-	
+	@Getter @Setter
+	private List<Produto> produtos;	
 
 }
